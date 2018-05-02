4112975f2def150344da4e4ff169e5ede6fe566c
Taku Funami <fff273109@gmail.com>
2018-05-02 21:07:38 +0900

    Lesson4 発展課題の再提出
    Test.cs の class Boss が魔法攻撃をする条件を変更しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -16,13 +16,10 @@ public class Test : MonoBehaviour {
         //魔法攻撃の関数を呼び出す
         lastboss.Magic(5);
         //魔法攻撃の関数を10回呼び出す
-        for (int mp = 0; mp < 10; mp++)
+        for ( int mp = 0; mp < 10; mp++)
         {
             lastboss.Magic(5);
         }
-
-
-
     }
 
     // Update is called once per frame
@@ -52,19 +49,24 @@ public class Boss
         this.hp -= damege;
     }
 
+
     //mpを消費して魔法攻撃する関数
     public void Magic(int magicalattack)
 
     {
-        //MPを消費して魔法攻撃をする
-        this.mp -= magicalattack;
-        Debug.Log("魔法攻撃をした。残りMPは" + mp);
 
-        if (mp <= 0)
+        if (mp - magicalattack <= 0)
         {
-            //  mpが0以下の場合、「MPが足りないため魔法が使えない。」と表示する
+            //魔法攻撃をしようとした際に mpが0以下になる場合、「MPが足りないため魔法が使えない。」と表示する
             Debug.Log("MPが足りないため魔法が使えない。");
         }
+
+        if (mp - magicalattack > 0)
+            //MPが0より多ければMPを消費して魔法攻撃をする
+            this.mp -= magicalattack;
+        Debug.Log("魔法攻撃をした。残りMPは" + mp);
+
+
     }
 
 
